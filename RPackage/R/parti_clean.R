4642b56d2bc89b0d14b44288da05e68daca56ffa
Måns Magnusson <mons.magnusson@gmail.com>
2017-05-29 20:33:37 +0200

    talare_parti

--- RPackage/R/parti_clean.R
+++ RPackage/R/parti_clean.R
@@ -8,6 +8,7 @@ parti_clean <- function(parti, talare){
   checkmate::assert_factor(talare)
   
   parti <- as.character(parti)
+  talare <- as.character(talare)
   parti <- tolower(parti)
   
   parti[parti=="kds"] <- "kd"
@@ -18,8 +19,14 @@ parti_clean <- function(parti, talare){
   talare_parti <- stringr::str_extract(talare, pattern = "\\(.+\\)")
   talare_parti <- stringr::str_extract(talare_parti, pattern = "[a-z]+")
   talare_parti[is.na(talare_parti)] <- "null"
+  
+  talare_parti[talare_parti=="kds"] <- "kd"
+  talare_parti[talare_parti=="fp"] <- "l"
+  talare_parti[talare_parti=="-"] <- "null"
+  
   diff_idx <- which(talare_parti!=parti)
   warning(length(diff_idx), " differences in party between variable 'talare' and 'parti'.")
+  # x <- dplyr::data_frame(parti = parti[diff_idx], talare_parti = talare_parti[diff_idx], talare = talare[diff_idx])
   parti[diff_idx] <- talare_parti[diff_idx]
   
   parti <- as.factor(parti)
