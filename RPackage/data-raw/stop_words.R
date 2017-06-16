13d249ce5a9847ce064786700886a3b39a3a4d5a
Måns Magnusson <mons.magnusson@gmail.com>
2017-06-16 15:32:43 +0200

    Force stop words to be UTF-8

--- RPackage/data-raw/stop_words.R
+++ RPackage/data-raw/stop_words.R
@@ -1,5 +1,6 @@
 # Store stop words
-stop_words_se <- readLines("RPackage/data-raw/stopwords/se.txt")
+stop_words_se <- readLines("RPackage/data-raw/stopwords/se.txt", encoding = "UTF-8")
+
 attr(stop_words_se, "created_date") <- Sys.time() 
 attr(stop_words_se, "created_git_hash") <- rcrpsriksdag:::get_git_sha1()
 
