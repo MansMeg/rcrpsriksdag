023242dbafeb2eeed388d0a3d8eada102797aa16
Måns Magnusson <mons.magnusson@gmail.com>
2017-06-25 22:56:57 +0200

    Updated test suite

--- RPackage/tests/testthat/test-speeches.R
+++ RPackage/tests/testthat/test-speeches.R
@@ -10,11 +10,22 @@ test_that("Dash exist", {
 })
 
 
-test_that("No punctuation exist", {
+test_that("No punctuation exist but underscore", {
   txt <- speeches$anforandetext
-  expect_true(!any(stringr::str_detect(txt, "\\.")))
-  expect_true(!any(stringr::str_detect(txt, ",|!")))
-  expect_true(!any(stringr::str_detect(txt, "\\?")))
+  txt <- stringr::str_replace_all(txt, "_", " ")
+  expect_true(!any(stringr::str_detect(txt, "[:punct:]")))
 })
+
+
+test_that("All collocation files has been handled", {
+  txt <- speeches$anforandetext
+  expect_true(any(stringr::str_detect(txt, "förväntad_livslängd"))) # collocations_2gram.txt
+  expect_true(any(stringr::str_detect(txt, "skylla_sig_själv"))) # collocations_3gram.txt
+  expect_true(any(stringr::str_detect(txt, "m_s"))) # collocations_manual.txt
+  expect_true(any(stringr::str_detect(txt, "ali_esbati"))) # collocations_mep.txt
+  expect_true(any(stringr::str_detect(txt, "västra_götalands_län"))) # collocations_region_names.txt
+  expect_true(any(stringr::str_detect(txt, "hbt_personers"))) # dash_collocations.csv
+})
+
 
 
