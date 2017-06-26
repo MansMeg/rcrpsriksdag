3ea2cb6fec1280d73e3b9812e7602dc4039bab71
Måns Magnusson <mons.magnusson@gmail.com>
2017-06-26 11:38:08 +0200

    Handle duplications in collocation txt files

--- RPackage/R/anforandetext_replace_collocations.R
+++ RPackage/R/anforandetext_replace_collocations.R
@@ -7,6 +7,7 @@
 #' 
 #' @examples 
 #' collocation_folder <- "RPackage/data-raw/collocations/"
+#' txt <- speeches$anforandetext
 #' anforandetext_replace_collocation(txt, collocation_folder)
 #' 
 #' @keywords Internal
@@ -15,13 +16,11 @@ anforandetext_replace_collocation <- function(anforandetext, collocation_folder)
   checkmate::assert_directory_exists(collocation_folder)
 
   raw <- read_in_collocation_files(collocation_folder)
-  
-  for(i in seq_along(raw)){
-    from <- paste0("(^| )", raw[[i]], "($| )")
-    to <- paste0(" ", stringr::str_replace_all(raw[[i]], " ", "_"), " ")
-    for(j in seq_along(to)){
-      anforandetext <- stringr::str_replace_all(anforandetext, from[j], to[j])
-    }
+
+  from <- paste0("(^| )", raw, "($| )")#[1:200]
+  to <- paste0(" ", stringr::str_replace_all(raw, " ", "_"), " ")#[1:200]
+  for(i in seq_along(to)){
+    anforandetext <- stringr::str_replace_all(anforandetext, from[i], to[i])
   }
 
   # Trim 
@@ -48,6 +47,9 @@ read_in_collocation_files <- function(collocation_folder){
     raw[[i]] <- readLines(collocation_files[i])
     checkmate::assert_character(raw[[i]], min.chars = 3, pattern = " ")
   }
-  raw
+  raw <- do.call(c,raw)
+
+  # Remove duplicates
+  raw[!duplicated(raw)]
 }
 
