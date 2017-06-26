6e25d1e5e4ee56bb363f457596c56de9a31a6fa1
Måns Magnusson <mons.magnusson@gmail.com>
2017-06-26 11:37:48 +0200

    Added replacing dashes

---
+++ RPackage/R/anforandetext_replace_dash_tokens.R
@@ -0,0 +1,24 @@
+#' Replace dashed tokens in anforandetext
+#' 
+#' @details 
+#' This function handles dashes used in the corpus. 
+#' There are two main uses for dashes: 
+#' 1) New lines that cuts words and 
+#' 2) Combined words
+#' 
+#' @param anforandetext a character vector from anforandetext in corpus file
+#' 
+#' @keywords Internal
+anforandetext_replace_dash_tokens <- function(speeches){
+  checkmate::assert_class(speeches, "tbl_df")
+
+  # Replace known errors
+  anforandetext <- anforandetext_replace_token_errors(speeches, "RPackage/data-raw/dash_errors/")
+
+  # Combine the rest of the dash separations
+  speeches$anforandetext <- stringr::str_replace_all(speeches$anforandetext, "-", "")
+  speeches$anforandetext <- stringr::str_replace_all(speeches$anforandetext, "[:space:]+", " ")
+  
+  speeches
+}
+
