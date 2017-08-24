6328f0191a84c4c9e9bd3238f975caff2421060a
Måns Magnusson <mons.magnusson@gmail.com>
2017-08-24 11:12:23 +0200

    Added data documentation

--- RPackage/R/data_documentation.R
+++ RPackage/R/data_documentation.R
@@ -3,12 +3,23 @@
 #' @details 
 #' Raw data containing debates in the Swedish parliament.
 #' @format A \code{tbl_df} with one row debate speech.
-#' @seealso \link{speeches}
+#' @seealso \link{speeches} \link{speeches_sentence_raw}
 #' @name speeches_raw
 #' @docType data
 #' @keywords data
 NULL
 
+#' Debates by sentence in the Swedish parliament (raw corpus)
+#'
+#' @details 
+#' Raw data containing debates in the Swedish parliament. 
+#' The debates has been cut into sentences using the Gavagai API.
+#' @format A \code{tbl_df} with one row per sentence in debate speeches.
+#' @seealso \link{speeches_sentence} \link{speeches_raw}
+#' @name speeches_sentence_raw
+#' @docType data
+#' @keywords data
+NULL
 
 #' Debates in the Swedish parliament (cleaned corpus)
 #'
@@ -20,6 +31,21 @@ NULL
 #' @format A \code{tbl_df} with one row debate speech.
 #' @seealso \link{speeches_raw}
 #' @name speeches
+#' @docType data
+#' @keywords data
+NULL
+
+#' Debates in the Swedish parliament (cleaned corpus)
+#'
+#' @details 
+#' A corpus with one sentence per row. 
+#' The debates has been cut into sentences using the Gavagai API.
+#' This is a cleaned version of \code{speeches_sentence_raw}, see  
+#' \code{RPackage/data-raw/gavagai.R} on how the data has been cleaned.
+#' 
+#' @format A \code{tbl_df} with one row debate speech.
+#' @seealso \link{speeches_sentence_raw} \link{speeches}
+#' @name speeches_sentence
 #' @docType data
 #' @keywords data
 NULL
